f48e014b495b4ed64c4f5ed3d3ade7cd87d8f023
shichia22 <shichia0207@gmail.com>
2021-09-21 19:06:26 +0800

    feat: 新增 user setting（尚未完成）

--- src/apis/user.js
+++ src/apis/user.js
@@ -7,6 +7,17 @@ export default {
       headers: { Authorization: `Bearer ${getToken()}` },
     })
   },
+  // TODO: 待修復 setting api
+  // getUserDetail() {
+  //   return apiHelper.get(`/users/${user_id}`, {
+  //     headers: { Authorization: `Bearer ${getToken()}` },
+  //   })
+  // },
+  updateUserSetting({ user_id, formData }) {
+    return apiHelper.put(`/users/${user_id}/setting`, formData, {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    })
+  },
   // addFollowed({ userId }) {
   //   return apiHelper.post(`/following/${userId}`, null, {
   //     headers: { Authorization: `Bearer ${getToken()}` },
